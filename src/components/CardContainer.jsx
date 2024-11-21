1e8340494eb7ee56ed2d3cd7349b6bf454d9fefb
mrashed21 <muhammadrashed760@gmail.com>
2024-11-21 19:27:57 +0600

    update spinar style

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,21 +1,24 @@
 import { useContext, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { CategoryContext } from "../provider/AllDataContext";
-import { AuthContext } from "../provider/AuthProvider";
 import CardDetails from "./CardDetails";
 
 const CardContainer = () => {
   const data = useLoaderData();
   const { selectedCategory } = useContext(CategoryContext);
-  const { setLoading } = useContext(AuthContext);
+  const [loading, setLoading] = useState(false);
   const [showAll, setShowAll] = useState(false);
+
   const handleSeeMore = () => {
+    const currentScrollPosition = window.scrollY;
     setLoading(true);
     setTimeout(() => {
       setShowAll(true);
       setLoading(false);
-    }, 3000);
+      window.scrollTo(0, currentScrollPosition);
+    }, 4000);
   };
+
   const filteredData = selectedCategory
     ? data.filter((card) => card.categoryName === selectedCategory)
     : data;
@@ -23,20 +26,27 @@ const CardContainer = () => {
   const displayedData = showAll ? filteredData : filteredData.slice(0, 9);
 
   return (
-    <div className="w-11/12 mx-auto my-20 md:px-20  ">
+    <div className="w-11/12 mx-auto my-20 md:px-20">
       <h1 className="text-center text-4xl font-bold mb-10">
         Eco-Adventure Experiences
       </h1>
-      <div className="space-y-5">
-        {displayedData.map((singleCard) => (
-          <CardDetails key={singleCard.id} singleCard={singleCard} />
-        ))}
-      </div>
+
+      {loading ? (
+        <div className="flex justify-center items-center h-screen">
+          <span className="loading loading-spinner loading-lg"></span>
+        </div>
+      ) : (
+        <div className="space-y-5">
+          {displayedData.map((singleCard) => (
+            <CardDetails key={singleCard.id} singleCard={singleCard} />
+          ))}
+        </div>
+      )}
 
       {!showAll && filteredData?.length > 9 && (
         <div className="mt-6 text-center px-10">
           <button
-            onClick={() => handleSeeMore()}
+            onClick={handleSeeMore}
             className="w-full btn btn-primary text-white font-semibold rounded-full text-lg"
           >
             See More
