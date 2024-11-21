e349b68ee3ce10ff015eeb90fcb238cb1eb547a8
mrashed21 <muhammadrashed760@gmail.com>
2024-11-21 09:13:49 +0600

    chnage on about us page

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,9 +1,9 @@
 const AboutUs = () => {
   return (
     <div>
-      <div className="bg-purple-100 min-h-screen py-10">
+      <div className="bg-purple-100 py-10">
         <h2 className="text-center text-3xl font-bold"> About us</h2>
-        <div className=" flex items-center justify-center h-screen">
+        <div className=" flex items-center justify-center w-full min-h-[450px]">
           <h2 className="text-3xl font-medium">No Information Found</h2>
         </div>
       </div>
